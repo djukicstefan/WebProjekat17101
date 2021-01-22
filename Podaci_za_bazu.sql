4ddbefca5c63f6b02f4ae6b942af91b7190dba44
djukicstefan <stefan.djkic@elfak.rs>
2021-01-22 09:55:46 +0100

    Initial commit

--- Podaci_za_bazu.sql
+++ Podaci_za_bazu.sql
@@ -1,57 +1,57 @@
 INSERT INTO Luka 
 DEFAULT VALUES;
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (500, 1, 300, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (500, 1, 300, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (1000, 0, 1000, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (1000, 0, 1000, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (1500, 0, 1500, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (1500, 0, 1500, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (5000, 0, 5000, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (5000, 0, 5000, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (6000, 6, 4650, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (6000, 6, 4650, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (6000, 3, 4000, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (6000, 3, 4000, 1);
 
-INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
-VALUES (4900, 5, 3000, 1);
+--INSERT INTO Brod (MaxKapacitet, BrojUpotrebljivihKontejnera, TrenutnaZauzetost, LukaID) 
+--VALUES (4900, 5, 3000, 1);
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Hrana', 100, 'H', 8); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Hrana', 100, 'H', 8); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Gorivo', 300, 'G', 7); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Gorivo', 300, 'G', 7); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Vakcine', 300, 'V', 8); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Vakcine', 300, 'V', 8); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Gorivo', 1200, 'G', 9); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Gorivo', 1200, 'G', 9); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Gorivo', 300, 'G', 9); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Gorivo', 300, 'G', 9); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Vakcine', 5000, 'V', 15); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Vakcine', 5000, 'V', 15); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Hrana', 300, 'H', 16); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Hrana', 300, 'H', 16); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Vakcine', 600, 'V', 16); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Vakcine', 600, 'V', 16); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Gorivo', 3000, 'G', 16); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Gorivo', 3000, 'G', 16); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Vakcine', 4000, 'V', 17); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Vakcine', 4000, 'V', 17); 
 
-INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
-VALUES ('Hrana', 3000, 'H', 18); 
+--INSERT INTO Kontejner (Ime, Tezina, Oznaka, BrodID)
+--VALUES ('Hrana', 3000, 'H', 18); 
 
